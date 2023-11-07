eb77ba6ef30a4da15f733a95e2c7d8c20c65dec4
ipekbal <mib208@exeter.ac.uk>
2023-11-07 17:59:49 +0000

    file methods changed

--- .src/Card.java
+++ .src/Card.java
@@ -1,9 +1,12 @@
 public class Card {
+    private int cardID;
     private int value;
 
     public static CardList<Card> cards = new CardList<>();
     public Card(int value) {
+        this.cardID = cards.size();
         this.value = value;
+
     }
     public int getValue() {return value;}
 
