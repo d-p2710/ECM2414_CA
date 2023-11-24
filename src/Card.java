ef30f04793ad1b87c82e6947f9f290c7e4dcbe60
d-p2710 <111011417+d-p2710@users.noreply.github.com>
2023-11-24 01:54:57 +0000

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,18 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
-//
-
 public class Card {
     private int cardID;
     private int value;
-    private static int totalCardNumber = 0;
-  
-    public static List<Card> cards = new ArrayList<>();
-    public Card(int value) {
-        this.cardID = totalCardNumber++;
+    public Card(int cardID, int value) {
+        this.cardID = cardID;
         this.value = value;
-        cards.add(this);
     }
-    public int getCardID(){return cardID;}
     public int getValue() {return value;}
+
+    public boolean compareTo(Card o) {
+        if (this.getValue() == o.getValue()) {
+            return true;
+        }
+        return false;
+    }
+
+    public String toString() {
+        return "Card [cardNumber=" + cardID + ", cardValue=" + value + "]";
+    }
+
 }
