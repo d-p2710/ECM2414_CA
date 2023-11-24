6977f90dc4e427a44f016db595027a965ea85332
d-p2710 <111011417+d-p2710@users.noreply.github.com>
2023-11-24 01:54:35 +0000

    Update CardDeck.java

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -1,17 +1,58 @@
 import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
-//
+
 public class CardDeck {
-
     private final int deckID;
-    private List<Card>cards;
+    private ArrayList<Card> deck;
+    private FileWriter outputFile;
 
-    public CardDeck(int deckID) {
+    public CardDeck(int deckID) throws IOException{
         this.deckID = deckID;
-        this.cards=new ArrayList<>();
+        this.deck = new ArrayList<>();
+        String outputFileName = "Deck" + deckID + "_output.txt";
+        this.outputFile = new FileWriter(outputFileName);
+    }
+    public int getDeckID(){return deckID;}
+    public ArrayList<Card> getDeck(){return deck;}
+
+    public void addCardToDeck(int index,Card card) {
+        if (index >= deck.size()){
+            this.deck.add(card);
+        } else {
+            this.deck.add(index, card);
+        }
+    }
+    public void recordFinalState(List<Card> finalCards) {
+        StringBuilder output = new StringBuilder("deck" + deckID + " contents: ");
+        for (Card card : finalCards) {
+            output.append(card.getValue()).append(",");
+        }
+        if(!finalCards.isEmpty()){
+            output.deleteCharAt(output.length()-1);
+        }
+        writeToOutputFile(output.toString());
     }
 
-    public void addCard(Card card) {
-        cards.add(card);
+    private void writeToOutputFile(String output) {
+        try {
+            outputFile.write(output + "\n");
+            outputFile.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeOutputFile() {
+        try {
+            outputFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String toString(){
+        return "CardDeck [cardDeck Number "+deckID+"\nDeck: "+deck+"]";
     }
 }
