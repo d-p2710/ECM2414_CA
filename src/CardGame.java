70ebb97ba68497b71ff42ccdfdb45336adcfe9a5
mrvipekbal <63160725+mrvipekbal@users.noreply.github.com>
2023-11-24 16:48:39 +0000

    Update CardGame.java

--- src/CardGame.java
+++ src/CardGame.java
@@ -4,40 +4,35 @@ import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+import static java.lang.Integer.parseInt;
+
 public class CardGame {
     // CardGame is a singleton class as once instantiated we don't want multiple games running only one at a given time.
-    private static volatile CardGame instance;
+    private static CardGame instance;
+
     // Private constructor to prevent instantiation from outside
+    private CardGame() {
+    }
+
+    public static CardGame getInstance() {
+        if (instance == null) {
+            instance = new CardGame();
+        }
+        return instance;
+    }
+
     private static int numPlayers;
     private ArrayList<Player> players;
     private ArrayList<CardDeck> decks;
-    private static ArrayList<Integer> pack=new ArrayList<>();
-
-    private CardGame(int numPlayers){
-        CardGame.numPlayers = numPlayers;
-        players = new ArrayList<Player>();
-        decks = new ArrayList<CardDeck>();
-    }
-
-    public static CardGame getInstance(int numPlayers) {
-        /*uses the double-check locking idiom to reduce overhead of acquiring a lock by testing criterion without having the lock */
-        CardGame result = instance;
-        if (result == null) {
-            synchronized (CardGame.class) {
-                result = instance;
-                if (result == null) {
-                    instance = result = new CardGame(numPlayers);
-                }
-            }
-        }
-        return result;
-    }
+    private static ArrayList<Integer> pack = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         // Read the number of players from the command-line input
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of players: ");
         numPlayers = scanner.nextInt();
         String filePath;
+
+
         // Process the input pack file
         while (true) {
             // Get the location of the input file
@@ -53,7 +48,7 @@ public class CardGame {
             }
         }
         pack = readFileIntoPack(numPlayers,filePath);
-        CardGame game = CardGame.getInstance(numPlayers);
+        CardGame game = CardGame.getInstance();
         game.startGame(pack);
     }
 
@@ -66,7 +61,7 @@ public class CardGame {
                     return;
                 }
                 try {
-                    int value = Integer.parseInt(line);
+                    int value = parseInt(line);
                     // Process the value as needed
                     System.out.println("Read value: " + value);
                 } catch (NumberFormatException e) {
@@ -77,6 +72,7 @@ public class CardGame {
         }
     }
 
+
     private static ArrayList<Integer> readFileIntoPack(int n, String filePath) throws IOException{
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             for (int i = 0; i < 8 * n; i++) {
@@ -84,25 +80,26 @@ public class CardGame {
                 if (line == null) {
                     throw new IOException("Input pack file is incomplete.");
                 }
-                int value = Integer.parseInt(line);
+                int value = parseInt(line);
                 pack.add(value);
             }
         }
         return pack;
     }
 
+
     public void startGame(ArrayList<Integer> pack) {
         try {
+            players = new ArrayList<Player>();
+            System.out.println(players);
+            decks = new ArrayList<CardDeck>();
             initialisePlayersAndDecks(numPlayers);
             allocateCards(pack);
-            for (Player player : players) {
-                System.out.println("Player Initialised: "+player.getPlayerID());
+            for (Player player: players) {
                 Thread thread = new Thread(player);
                 thread.start();
-                System.out.println(player);
-            }
-            for (CardDeck cardDeck: decks){
-                System.out.println(cardDeck);
+                thread.join();
+                System.out.println(thread);
             }
 
         } catch (Exception e) {
@@ -110,55 +107,40 @@ public class CardGame {
         }
     }
 
-
-    private void initialisePlayersAndDecks(int numPlayers) throws IOException {
+    private void initialisePlayersAndDecks(int numPlayers) {
         for (int i = 0; i < numPlayers; i++) {
             Player player = new Player(i + 1);
             players.add(player);
             CardDeck deck = new CardDeck(i + 1);
             decks.add(deck);
         }
-        for (int i = 0; i <numPlayers; i++){
-            players.get(i).setLHSDeckId((i + numPlayers) % numPlayers + 1);
-            players.get(i).setRHSDeckId(i == 0 ? numPlayers : i + 1);
-        }
     }
 
     private void allocateCards(ArrayList<Integer> pack) {
         int index = 0;
-        int deckIndex = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < numPlayers; j++) {
-                Card card = new Card(index, pack.get(index));
-                System.out.println(index);
-                players.get(j).addCardToHand(i, card);
+        int deckIndex =0;
+        for (int i = 0; i < numPlayers; i++) {
+            System.out.println(index);
+            Card card = new Card(index, pack.get(index));
+            for (int j = 0; j < 4; j++) {
+                players.get(j).addCardtoHand(card);
                 index++;
             }
+
         }
-        for (int i = 0; i < pack.size() - (numPlayers * 4); i++) {
+        for (int i = 0; i < pack.size() - (numPlayers* 4); i++) {
             Card card = new Card(index, pack.get(index));
-            System.out.println("i:" + i);
-            decks.get(deckIndex).addCardToDeck(i % numPlayers, card);
+            decks.get(deckIndex).addCardtoDeck(card);
             index++;
-        deckIndex = (deckIndex + 1) % numPlayers; // Wrap around to 0 when it reaches the number of decks
+            deckIndex = (deckIndex + 1) % numPlayers;
         }
+        for (int i = 0; i <numPlayers; i++){
+            players.get(i).setLHSDeck(decks.get(i));
+            players.get(i).setRHSDeck(decks.get((i + 1)%numPlayers));
+        }
+        //System.out.println(players);
+        //System.out.println(decks);
     }
 
-    public static boolean checkWinCondition(Card[] hand) {
-        if (hand == null || hand.length == 0) {
-            // Handle edge cases, like an empty array or null reference
-            return false;
-        }
-
-        int firstNumber = hand[0].getValue();
-
-        for (int i = 1; i < hand.length; i++) {
-            if (hand[i].getValue() != firstNumber) {
-                // If any number is different, return false
-                return false;
-            }
-        }
-        // All numbers are the same
-        return true;
-    }
 }
+
