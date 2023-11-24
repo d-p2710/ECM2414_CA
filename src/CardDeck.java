f0a389611c1a90cd0c12d4447899e1e4b29e7f60
d-p2710 <111011417+d-p2710@users.noreply.github.com>
2023-11-24 17:24:07 +0000

    Update CardDeck.java

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -1,24 +1,54 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 public class CardDeck {
-
     private final int deckID;
     private ArrayList<Card> deck;
+    private FileWriter outputFile;
 
-    public CardDeck(int deckID) {
+    public CardDeck(int deckID) throws IOException{
         this.deckID = deckID;
-        this.deck = new ArrayList<Card>();
+        this.deck = new ArrayList<>();
+        String outputFileName = "Deck" + deckID + "_output.txt";
+        this.outputFile = new FileWriter(outputFileName);
     }
     public int getDeckID(){return deckID;}
-
     public ArrayList<Card> getDeck(){return deck;}
 
-    public void addCardtoDeck(Card card) {
+    public void addCardToDeck(int index,Card card) {
         deck.add(card);
     }
+    public void recordFinalState(List<Card> finalCards) {
+        StringBuilder output = new StringBuilder("deck" + deckID + " contents: ");
+        for (Card card : finalCards) {
+            output.append(card.getValue()).append(",");
+        }
+        if(!finalCards.isEmpty()){
+            output.deleteCharAt(output.length()-1);
+        }
+        writeToOutputFile(output.toString());
+    }
 
+    private void writeToOutputFile(String output) {
+        try {
+            outputFile.write(output + "\n");
+            outputFile.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeOutputFile() {
+        try {
+            outputFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String toString(){
+        return "CardDeck [cardDeck Number "+deckID+"\nDeck: "+deck+"]";
+    }
 }
