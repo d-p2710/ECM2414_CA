6657ca938b191a077b3bcdff37aeb2677ccb18b5
mrvipekbal <63160725+mrvipekbal@users.noreply.github.com>
2023-11-24 16:46:50 +0000

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,6 +1,7 @@
 public class Card {
     private int cardID;
     private int value;
+    //private static int totalCardNumber = 0;
   
 
     public Card(int cardID, int value) {
@@ -10,16 +11,11 @@ public class Card {
     public int getCardID(){return cardID;}
     public int getValue() {return value;}
 
-    public int compareTo(Card o)
-    {
-        if (this.getValue() == o.getValue()) {
-            return 0;
-        } else
-            return -1;
-    }
 
-    public String toString() {
+    public String toString()
+    {
         return "Card [cardNumber=" + cardID + ", cardValue=" + value + "]";
     }
 
 }
+
