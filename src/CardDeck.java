39a7e4190b6850482c0c24a537514acbaffdab36
mrvipekbal <63160725+mrvipekbal@users.noreply.github.com>
2023-11-25 17:18:02 +0000

    Update CardDeck.java

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -1,54 +1,38 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 public class CardDeck {
+
     private final int deckID;
     private ArrayList<Card> deck;
     private FileWriter outputFile;
 
-    public CardDeck(int deckID) throws IOException{
+    public CardDeck(int deckID) throws IOException {
         this.deckID = deckID;
-        this.deck = new ArrayList<>();
+        this.deck = new ArrayList<Card>();
         String outputFileName = "Deck" + deckID + "_output.txt";
         this.outputFile = new FileWriter(outputFileName);
     }
     public int getDeckID(){return deckID;}
+
     public ArrayList<Card> getDeck(){return deck;}
 
-    public void addCardToDeck(int index,Card card) {
+    public void addCardtoDeck(Card card) {
         deck.add(card);
     }
-    public void recordFinalState(List<Card> finalCards) {
-        StringBuilder output = new StringBuilder("deck" + deckID + " contents: ");
-        for (Card card : finalCards) {
-            output.append(card.getValue()).append(",");
-        }
-        if(!finalCards.isEmpty()){
-            output.deleteCharAt(output.length()-1);
-        }
-        writeToOutputFile(output.toString());
-    }
 
-    private void writeToOutputFile(String output) {
-        try {
-            outputFile.write(output + "\n");
-            outputFile.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public String toString() {
+        StringBuilder deckString = new StringBuilder("CardDeck " +deckID + "\nDeck: ");
+        for (Card card: deck) {
+            deckString.append(card.getValue()).append(",");
         }
-    }
-
-    public void closeOutputFile() {
-        try {
-            outputFile.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (!deck.isEmpty()) {
+            deckString.deleteCharAt(deckString.length()-1);
         }
-    }
-
-    public String toString(){
-        return "CardDeck [cardDeck Number "+deckID+"\nDeck: "+deck+"]";
+        return deckString.toString();
     }
 }
+
