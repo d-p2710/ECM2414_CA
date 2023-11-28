6f77122b34db62b4d795fea47f2ce4b6e31aa300
d-p2710 <111011417+d-p2710@users.noreply.github.com>
2023-11-28 19:04:45 +0000

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,7 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 //
 
 public class Player implements Runnable{
@@ -8,7 +9,6 @@ public class Player implements Runnable{
     public int preferredDenomination;
     private ArrayList<Card> hand;
     private FileWriter outputFile;
-    private int preferredCardCount;
     private CardDeck LHSDeck;
     private CardDeck RHSDeck;
     private volatile boolean gameOver;
@@ -34,32 +34,54 @@ public class Player implements Runnable{
     public void setRHSDeck(CardDeck RHSDeck) {this.RHSDeck = RHSDeck;}
     public void setLHSDeck(CardDeck LHSDeck) {this.LHSDeck = LHSDeck;}
 
-    public void addCardtoHand(Card card){
+    public void addCardToHand(Card card){
         this.hand.add(card);
     }
     public ArrayList<Card> getHand() {
         return hand;
     }
-    
-    public void updateOutputFile(){
+
+    public synchronized void updateOutputFile(){
         try{
-            this.outputFile.write("player " + this.playerID + " current hand is ");
+            outputFile.write("player " + this.playerID + " current hand is ");
             for (Card card : hand){
                 if (card != null){
                     outputFile.write(card.getValue()+" ");
                 }}
             //Flush ensures data is written immediately
-            this.outputFile.write("\n");
-
+            outputFile.write("\n");
+            outputFile.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void closeOutputFile(){
-        try{
-            outputFile.close();
-        }catch (IOException e){
+    public synchronized void recordWin() {
+        String output = "player " + playerID + " wins";
+        System.out.println(output);
+        writeToOutputFile(output);
+        gameOver = true;
+    }
+
+    public void recordFinalState(List<Card> finalCards) {
+        StringBuilder output = new StringBuilder("player" + playerID + " final hand: ");
+        int cardCount = finalCards.size();
+
+        for (int i = 0; i < cardCount; i++) {
+            output.append(finalCards.get(i).getValue());
+            if (i < cardCount - 1) {
+                output.append(", ");
+            }
+        }
+        writeToOutputFile("player" + playerID + " exits");
+        writeToOutputFile(output.toString());
+    }
+
+    private void writeToOutputFile(String output) {
+        try {
+            outputFile.write(output + "\n");
+            outputFile.flush();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -80,11 +102,11 @@ public class Player implements Runnable{
             synchronized (this) {
                 //doesnt draw a card while the LHSDeck is empty
                 while (deck.isEmpty()) {
-                    wait();
+                    wait(100);
                 }
                 //removes the card drawn from the LHSDeck
                 Card drawnCard = deck.remove(0);
-                System.out.println("\nplayer " + this.playerID + " draws a " + drawnCard.getValue() + " from deck " + deckID + "\n");
+                writeToOutputFile("\nplayer " + this.playerID + " draws a " + drawnCard.getValue() + " from deck " + deckID + "\n");
                 //outputFile.write("player " + this.playerID + " draws a " + drawnCard + " from deck " + deckID + "\n");
                 //adds the card drawn to the hand
                 hand.add(drawnCard);
@@ -104,15 +126,15 @@ public class Player implements Runnable{
         hand.remove(discardCardIndex);
         //adds the discarded card to RHSDeck
         deck.add(cardToDiscard);
-
-        System.out.println("\nplayer "+this.playerID + " discards a " + cardToDiscard.getValue() + " to deck " + deckID + "\n");
+        notifyAll();
+        writeToOutputFile("\nplayer "+this.playerID + " discards a " + cardToDiscard.getValue() + " to deck " + deckID + "\n");
         if(checkWinCondition(hand)) {
             notifyAll();
         }
         //outputFile.write("player "+this.playerID + " discards a " + cardToDiscard + " to deck " + deckID + "\n");
         return cardToDiscard;
     }
-    public static boolean checkWinCondition(ArrayList<Card> hand) {
+    public boolean checkWinCondition(ArrayList<Card> hand) {
         if (hand == null || hand.size() == 0) {
             // Handle edge cases, like an empty array or null reference
             return false;
@@ -126,8 +148,8 @@ public class Player implements Runnable{
                 return false;
             }
         }
-
         // All numbers are the same
+        recordWin();
         return true;
     }
     @Override
@@ -163,5 +185,3 @@ public class Player implements Runnable{
     }
 
 }
-
-// Maybe shuffle cards, otherwise when using th same deck the result of the game could be very predictable & consistent, therefore cards need to be randomised.
