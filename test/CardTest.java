14451b714e4c2f5cdb0c3b0489718504781c03f5
d-p2710 <111011417+d-p2710@users.noreply.github.com>
2023-11-29 12:53:39 +0000

    Update CardTest.java

--- test/CardTest.java
+++ test/CardTest.java
@@ -8,15 +8,9 @@ public class CardTest {
         assertEquals(5, card.getValue());
     }
     @Test
-    public void testComparisonEquals(){
-        Card card1=new Card(0,5);
-        Card card2=new Card(1,5);
-        assertTrue(card1.compareTo(card2));
-    }
-    @Test
-    public void testComparisonNotEqual(){
-        Card card1=new Card(0,5);
-        Card card2=new Card(1,6);
-        assertFalse(card1.compareTo(card2));
+    public void testToString(){
+        Card card= new Card(1,10);
+        String expectedString="Card [cardNumber=1, cardValue=10]";
+        assertEquals(expectedString, card.toString());
     }
 }
