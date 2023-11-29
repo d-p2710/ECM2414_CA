fb43e0f14c454044c18f2db893257d59b0810e90
d-p2710 <111011417+d-p2710@users.noreply.github.com>
2023-11-29 13:53:33 +0000

    Update CardGameTest.java

--- test/CardGameTest.java
+++ test/CardGameTest.java
@@ -1,56 +1,18 @@
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class CardGameTest {
     @Test
-    public void testLoadAndValidatePackFile_validFile(){
+    public void testGetInstance(){
+        CardGame instance1 = CardGame.getInstance();
+        CardGame instance2 = CardGame.getInstance();
 
+        assertSame(instance1, instance2);
     }
-    @Test
-    public void testLoadAndValidatePackFile_nonExistentFile() {
 
-    }
-    @Test
-    public void testLoadAndValidatePackFile_invalidContent(){
-
-    }
-    @Test
-    public void testLoadAndValidatePackFile_insufficientLines(){
-
-    }
-    @Test
-    public void roundRobinAllocation(){
-
-    }
-    @Test
-    public void roundRobinPlayersThenCardDecks(){}
-    @Test
-    public void cardDecksAppendedToDecks(){}
-    @Test
-    public void testPlayerAndDeckInitialisation(){
-        CardGame game = CardGame.getInstance();
-    }
-    @Test
-    public void testThreadsStartConcurrently(){
-        /*game play should start concurrently, initially checks cards dealt
-        if a winning hand as all the same value if not threads start to play
-        the game drawing & discarding no-preferred denomination cards until
-        all hand == preferred denomination.
-         */
-    }
-    @Test
-    public void testPlayGame(){}
-
-    @Test
-    public void decksArrayList(){
-        /* Should be an ArrayList of cardDecks in the format of
-        [cardDeckID0: [card1.getValue(), card2.getValue()...card_.getValue()],
-        cardDeckID1: [card1.getValue(), card2.getValue()...card_.getValue()],
-        cardDeckID2: [card1.getValue(), card2.getValue()...card_.getValue()]]
-         */
-        }
-    }
+}
